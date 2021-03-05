18147c09c0699601752273cc5572adf7ccedb280
MadhushaPrasad <madushaprasad21@gmail.com>
2021-03-05 18:44:26 +0530

    Realtime Chat Application

---
+++ chatapp.sql
@@ -0,0 +1,46 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+CREATE TABLE messages
+(
+    msg_id          int(11)       NOT NULL,
+    incoming_msg_id int(255)      NOT NULL,
+    outgoing_msg_id int(255)      NOT NULL,
+    msg             varchar(1000) NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+
+CREATE TABLE users
+(
+    user_id   int(11)      NOT NULL,
+    unique_id int(255)     NOT NULL,
+    fname     varchar(255) NOT NULL,
+    lname     varchar(255) NOT NULL,
+    email     varchar(255) NOT NULL,
+    password  varchar(255) NOT NULL,
+    img       varchar(255) NOT NULL,
+    status    varchar(255) NOT NULL
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4;
+
+ALTER TABLE messages
+    ADD PRIMARY KEY (msg_id);
+
+ALTER TABLE users
+    ADD PRIMARY KEY (user_id);
+
+ALTER TABLE messages
+    MODIFY msg_id int(11) NOT NULL AUTO_INCREMENT;
+
+ALTER TABLE users
+    MODIFY user_id int(11) NOT NULL AUTO_INCREMENT;
+COMMIT;
+
+
+INSERT INTO users(user_id, unique_id, fname, lname, email, password, img, status) value (1, "Madhusha", "Prasad",
+                                                                                         "madushaprasad21@gmail.com",
+                                                                                         "1234",
+                                                                                         "/php/images/profile.jpg",
+                                                                                         "active");
